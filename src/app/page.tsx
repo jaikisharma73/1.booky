d9b897d80bd272b192f483f9e27223a5cfeb7d9b
Jaiki Sharma <jack912062sharmas@gmail.com>
2026-06-09 23:38:58 +0530

    h

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,8 @@ export default function Home() {
   return (
     <div className="min-h-screen flex flex-col justify-center text-center items-center">
       <h1>V Booky </h1>
-      <p>Smart Vehical Booking Plateform</p>
-      <p>Book your vehical</p>
+      <p>The best platform for booking your vehical</p>
+      <p>Book your vehical in a few clicks</p>
     </div>
   );
 }
